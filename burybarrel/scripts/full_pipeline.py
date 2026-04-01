90a7e304906b1aa6a0434229e2593d7bd7cb624f
Jerry Yan <yan569@purdue.edu>
2026-03-31 21:57:57 -0400

    stop RUNNING OUT OF MEMORY

--- burybarrel/scripts/full_pipeline.py
+++ burybarrel/scripts/full_pipeline.py
@@ -1,5 +1,7 @@
 import concurrent.futures
 from pathlib import Path
+import subprocess
+import sys
 import time
 import traceback
 
@@ -120,7 +122,7 @@ def run_full_pipelines(names, datadir, resdir, objdir, devices=None, step_all=Fa
         alldatadirs = filter(lambda x: x.is_dir() and (x / "info.json").exists(), datadir.glob("*"))
         names = [x.name for x in alldatadirs]
         # temporary checkpoint lol
-        names = ['dive8-barrel-14-32', 'dive8-barrel-15-19-09', 'dive8-barrel-16-31', 'dive10-barrel-03-40', 'dive9-barrel-03-17', 'dive8-barrel-13-08', 'dive8-barrel-16-30', 'dive8-barrel-11-33', 'dive6-smokefloat-10-20', 'dive8-barrel-14-37', 'dive8-barrel-10-56', 'dive8-barrel-14-05', 'dive10-barrel-03-55', 'dive8-barrel-15-04', 'dive9-barrel-03-47', 'dive9-barrel-03-45', 'dive8-barrel-15-19-49', 'dive8-barrel-15-09-43', 'barrel1', 'dive8-barrel-15-24', 'dive8-barrel-16-19', 'dive9-barrel-03-13', 'dive8-barrel-15-14', 'dive10-barrel-04-35', 'dive8-barrel-10-24', 'dive9-barrel-04-07', 'dive8-barrel-11-12', 'dive6-smokefloat-10-17-26', 'dive3-depthcharge-03-04', 'dive8-barrel-10-45', 'dive8-barrel-15-50', 'dive3-depthcharge-05-11', 'dive8-barrel-11-04', 'dive10-barrel-03-43', 'dive8-barrel-10-49', 'dive8-barrel-13-07', 'dive8-barrel-16-09', 'barrel2', 'dive9-barrel-03-39', 'dive8-barrel-14-29', 'dive8-barrel-12-55', 'dive8-barrel-13-57', 'dive8-barrel-11-21', 'dive8-barrel-14-51', 'dive8-barrel-16-18']
+        names = ['dive9-barrel-03-45', 'dive8-barrel-15-19-49', 'dive8-barrel-15-09-43', 'barrel1', 'dive8-barrel-15-24', 'dive8-barrel-16-19', 'dive9-barrel-03-13', 'dive8-barrel-15-14', 'dive10-barrel-04-35', 'dive8-barrel-10-24', 'dive9-barrel-04-07', 'dive8-barrel-11-12', 'dive6-smokefloat-10-17-26', 'dive3-depthcharge-03-04', 'dive8-barrel-10-45', 'dive8-barrel-15-50', 'dive3-depthcharge-05-11', 'dive8-barrel-11-04', 'dive10-barrel-03-43', 'dive8-barrel-10-49', 'dive8-barrel-13-07', 'dive8-barrel-16-09', 'barrel2', 'dive9-barrel-03-39', 'dive8-barrel-14-29', 'dive8-barrel-12-55', 'dive8-barrel-13-57', 'dive8-barrel-11-21', 'dive8-barrel-14-51', 'dive8-barrel-16-18']
     # round robin assignment of datasets to devices
     for i, name in enumerate(names):
         devicetaskdict[devices[i % ndevices]].append(name)
@@ -151,7 +153,6 @@ def run_full_pipelines(names, datadir, resdir, objdir, devices=None, step_all=Fa
 
 def _run_full_pipeline(name, datadir, resdir, objdir, device=None, step_mask=False, step_foundpose=False, step_fit=False):
     from burybarrel.foundpose_fit import load_fit_write
-    from burybarrel.scripts.create_masks import _create_masks
     from burybarrel.scripts.run_foundpose import _run_foundpose
     datadir = Path(datadir) / name
     resdir = Path(resdir) / name
@@ -165,7 +166,21 @@ def _run_full_pipeline(name, datadir, resdir, objdir, device=None, step_mask=Fal
     maskdir = resdir / "sam-masks"
     if step_mask:
         mask_t0 = time.time()
-        _create_masks(imgdir, text_prompt, maskdir, closekernelsize=5, convexhull=True, device=device)
+        runcmd = [
+            sys.executable, "-m", "burybarrel", "create-masks",
+            "-i", str(imgdir),
+            "-p", text_prompt,
+            "-o", str(maskdir),
+            "--closekernel", "5",
+            "--convexhull",
+        ]
+        if device is not None:
+            runcmd += ["-d", device]
+        try:
+            subprocess.run(runcmd, check=True, stderr=sys.stderr, stdout=sys.stdout)
+        except subprocess.CalledProcessError as e:
+            logger.error(f"Error with create-masks:\n{e}")
+            raise
         mask_t1 = time.time()
         logger.info(f"Mask creation for dataset {name} took {mask_t1 - mask_t0:.2f} seconds")
     if step_foundpose:
@@ -197,4 +212,5 @@ def _run_pipelines_gpu(names, datadir, resdir, objdir, device=None, step_mask=Fa
             _run_full_pipeline(name, datadir, resdir, objdir, device=device, step_mask=step_mask, step_foundpose=step_foundpose, step_fit=step_fit)
         except Exception as e:
             logger.error(f"ERROR IN RUNNING {name} with exception: {e}\n{traceback.format_exc()}\nContinuing to next dataset")
-        torch.cuda.empty_cache()
+        finally:
+            torch.cuda.empty_cache()
